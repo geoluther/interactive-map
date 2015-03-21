94598fecc634de8c80f28e1c273cda1e221df1df
George <my latest commit>
2015-03-20 19:50:31 -0600

    add list placeholder

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+var myCenter = new google.maps.LatLng(48.1484973,11.5727348);
 
 function initialize() {
   var mapProp = {
@@ -18,3 +18,26 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+var model = [
+  {
+    name: "My Favorite Restaurant",
+    LatLng: ["48.1484973", "11.5727348"]
+  },
+
+  {
+    name: "My Other Favorite Restaurant",
+    LatLng: ["49.1484973", "11.5727348"]
+  },
+
+  {
+    name: "Boulder Theater",
+    LatLng: ["40.0191931", "-105.2772349"]
+
+  }
+];
+
+var Location = function() {
+
+};
