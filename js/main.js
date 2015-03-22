177019cc81ca10828c3de52d1a5a1d42fdcd2717
George <my latest commit>
2015-03-21 22:44:06 -0600

    map init stuff and regexp

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 
-var myCenter = new google.maps.LatLng(40.019059,-105.277234);
+
 
 function initialize() {
+
+  var myCenter = new google.maps.LatLng(40.019059,-105.277234);
   var mapProp = {
     center: myCenter,
     zoom: 16,
@@ -10,39 +12,45 @@ function initialize() {
 
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-  var marker = new google.maps.Marker({
-    position: myCenter,
-  });
+  for (var i = 0; i < initialPlaces.length; i++) {
 
-  marker.setMap(map);
+    var name = initialPlaces[i].name;
+    var loc =  initialPlaces[i].LatLng;
+    var myLatLng  = new google.maps.LatLng(loc[0], loc[1]);
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: name,
+    });
+  }
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
 
 var initialPlaces = [
-{
-  name: "Bohemian Biergarten",
-  LatLng: [40.0187011,-105.2792224]
-},
+  {
+    name: "Bohemian Biergarten",
+    LatLng: [40.0187011, -105.2792224]
+  },
 
-{
-  name: "Mountain Sun",
-  LatLng: [40.019059,-105.277234]
-},
+  {
+    name: "Mountain Sun",
+    LatLng: [40.0189209,-105.2748623]
+  },
 
-{
-  name: "License #1",
-  LatLng: [40.019416,-105.2794457]
-},
+  {
+    name: "License #1",
+    LatLng: [40.019416, -105.2794457]
+  },
 
-{
-  name: "The Black Cat Bistro",
-  LatLng: [40.0178096,-105.2789281]
-},
+  {
+    name: "The Black Cat Bistro",
+    LatLng: [40.018012,-105.2781439]
+  },
 
-{
-  name: "Boulder Theater",
-  LatLng: [40.019059,-105.277234]
+  {
+    name: "Boulder Theater",
+    LatLng: [40.019059,-105.277234]
 }
 ];
 
@@ -64,9 +72,17 @@ var ViewModel =  function() {
     self.placeList.push(new Location(placeItem) );
   });
 
+  this.fliteredItems = ko.computed(function() {
+        var re = new RegExp(this.searchString(), "i");
+        console.log(re);
+        console.log(this.searchString());
+    }, this);
+
   this.doSomething = function(place){
     console.log(place.name);
   }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 };
 
